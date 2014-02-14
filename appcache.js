35b0551fd69872d43fb08741fb96cc5ed3f68c10
Tim Caswell <tim@creationix.com>
2014-02-14 03:24:51 +0000

    Update appcache filter to new format

--- appcache.js
+++ appcache.js
@@ -1,6 +1,9 @@
+"use strict";
+
 var sha1 = require('git-sha1');
 var pathJoin = require('pathjoin');
 var parallel = require('carallel');
+var binary = require('bodec');
 
 var mime = "text/cache-manifest";
 
@@ -8,33 +11,29 @@ var cache = {};
 
 module.exports = appcache;
 
-function appcache(req, callback) {
+function appcache(servePath, req, callback) {
   // If the file is external load and cache it for speed.
   if (req.target) {
     var cached = cache[req.target.hash];
     if (!cached) {
       return req.target.fetch(function (err, input) {
         if (input === undefined) return callback(err);
-        cache[req.target.hash] = ("" + input).split("\n").filter(Boolean);
-        return appcache(req, callback);
+        input = binary.toUnicode(input);
+        cache[req.target.hash] = input.split("\n").filter(Boolean);
+        return appcache(servePath, req, callback);
       });
     }
     req.args = req.args.concat(cached);
   }
 
-  render(req, function (err, manifest) {
-    if (err) return callback(err);
-    var etag = 'W/"' + sha1(manifest) + '"';
-    callback(null, {etag: etag, mime: mime, fetch:function (callback) {
-      callback(null, manifest);
-    }});
-  });
-}
 
-function render(req, callback) {
-  parallel(req.args.map(function (file) {
-    return req.repo.servePath(req.root, pathJoin(req.base, file));
-  }), function (err, entries) {
+  var actions = req.args.map(function (file) {
+    return function (callback) {
+      servePath(pathJoin(req.path, "..", file), null, callback);
+    };
+  });
+
+  parallel(actions, function (err, entries) {
     if (err) return callback(err);
     var manifest = "CACHE MANIFEST\n";
     entries.forEach(function(entry, i) {
@@ -45,6 +44,10 @@ function render(req, callback) {
         manifest += req.args[i] + "\n";
       }
     });
-    callback(null, manifest);
+    var etag = sha1(manifest);
+    callback(null, {etag: etag, mime: mime, fetch:function (callback) {
+      callback(null, manifest);
+    }});
   });
+
 }
