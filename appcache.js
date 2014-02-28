52bf626e6db6642297da67ce27db0f6ea44e8d33
Tim Caswell <tim@creationix.com>
2014-02-28 03:01:06 +0000

    Update amd filter to new .rule format

--- appcache.js
+++ appcache.js
@@ -3,33 +3,16 @@
 var sha1 = require('git-sha1');
 var pathJoin = require('pathjoin');
 var parallel = require('carallel');
-var binary = require('bodec');
 
 var mime = "text/cache-manifest";
-
-var cache = {};
 
 module.exports = appcache;
 
 function appcache(servePath, req, callback) {
-  // If the file is external load and cache it for speed.
-  if (req.target) {
-    var cached = cache[req.target.hash];
-    if (!cached) {
-      return req.target.fetch(function (err, input) {
-        if (input === undefined) return callback(err);
-        input = binary.toUnicode(input);
-        cache[req.target.hash] = input.split("\n").filter(Boolean);
-        return appcache(servePath, req, callback);
-      });
-    }
-    req.args = req.args.concat(cached);
-  }
 
-
-  var actions = req.args.map(function (file) {
+  var actions = req.cache.map(function (file) {
     return function (callback) {
-      servePath(pathJoin(req.path, "..", file), null, callback);
+      servePath(pathJoin(req.paths.rule, "..", file), callback);
     };
   });
 
@@ -37,15 +20,16 @@ function appcache(servePath, req, callback) {
     if (err) return callback(err);
     var manifest = "CACHE MANIFEST\n";
     entries.forEach(function(entry, i) {
-      if (entry) {
-        manifest += req.args[i] + "#" + entry.etag + "\n";
+      if (entry.hash) {
+        manifest += req.cache[i] + "#" + entry.hash + "\n";
       }
       else {
-        manifest += req.args[i] + "\n";
+        manifest += req.cache[i] + "\n";
       }
     });
-    var etag = sha1(manifest);
-    callback(null, {etag: etag, mime: mime, fetch:function (callback) {
+    // TODO: output data from "fallback" and "network" in the rule
+    var hash = sha1(manifest);
+    callback(null, {hash: hash, mime: mime, fetch:function (callback) {
       callback(null, manifest);
     }});
   });
